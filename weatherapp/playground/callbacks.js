9988b1d49ce7dc3036610a16a2f89f96b4793bcf
Andrew Nixon <andrewnixon0@gmail.com>
2017-08-26 20:49:07 +0100

    added callbacks.js

---
+++ weatherapp/playground/callbacks.js
@@ -0,0 +1,11 @@
+var getUser = (id, callback) => {
+  var user = {id: id, name:"Dude" };
+
+  setTimeout(() => {
+    callback(user)
+  }, 3000);
+};
+
+getUser(31, (user) =>{
+  console.log(user);
+});

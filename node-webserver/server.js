f8f7cdfc8e8d3e5d6703572200abe912de31fea8
andrewnixon0 <andrewnixon0@gmail.com>
2017-08-31 22:33:48 +0100

    Middleware test

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
@@ -7,18 +8,40 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+app.use((req, res, next) => {
+    now = new Date().toString();
+    log = `${now}: ${req.ip} - ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to write server log');
+        }
+    });
+    console.log(log);
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle:'Welcome Page',
-        currentYear: new Date().getFullYear(),
         welcomeMessage: 'This is a welcome message!!!'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle:'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle:'About Page'
     });
 })
 

c2744f10f8965e57282c3c77b61bea21b3dae75b
andrewnixon0 <andrewnixon0@gmail.com>
2017-10-07 22:59:46 +0100

    used Mocha and Expect to test

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,42 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('Should add two numbers', () => {
-    var res = utils.add(33, 11);
+    let res = utils.add(33, 11);
 
-    if (res !== 44){
-        throw new Error(`Expected 44, but got ${res}`);
-    }
+    expect(res).toBe(44).toBeA('number');
+});
+
+it('Should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+      expect(sum).toBe(7).toBeA('number');
+      done();
+    });
 });
 
 it('Should square a number', () => {
-    var res = utils.square(4);
+    let res = utils.square(4);
 
-    if (res !== 16){
-        throw new Error(`Expected 16, but got ${res}`);
-    }
+    expect(res).toBe(16).toBeA('number');
+});
+
+it('Should async square two numbers', (done) => {
+    utils.asyncSquare(4, (square) => {
+      expect(square).toBe(16).toBeA('number');
+      done();
+    });
+});
+
+it('Should verify first and last names are set', () => {
+  var user = {
+    location: 'birmingham',
+    age: 32
+  };
+
+  let res = utils.setName(user, 'Andrew Nixon');
+
+  expect(res).toEqual(user).toInclude({
+    firstName: 'Andrew',
+    lastName: 'Nixon'
+  });
 });

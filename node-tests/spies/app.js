307ddf26710071e95b54dc9d4a3706bdf3d1e609
andrewnixon0 <andrewnixon0@gmail.com>
2017-10-14 11:46:18 +0100

    Spies are working as expected

--- node-tests/spies/app.js
+++ node-tests/spies/app.js
@@ -1,5 +1,7 @@
+var db = require('./db.js');
+
 module.exports.handleSignup = (email, password) => {
-  //check if e-mail exists
-  //save user to db
-  //send welcome e-mail
+  // Check if email already exists
+  db.saveUser({email, password});
+  // Send the welcome email
 };

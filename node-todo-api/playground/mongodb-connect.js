27b2fee66735a8f061e0b2d6c261c645599bc7c5
Andrew Nixon <andrewnixon0@gmail.com>
2017-10-22 22:05:33 +0100

    Working DB writes.

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,5 +1,34 @@
 const MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err){
+    return console.log('There was an error. Unable to connect to server');
+  }
 
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops), undefined, 2);
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'Andrew',
+    age: 32,
+    location: 'UK'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert todo', err);
+    }
+
+    console.log(JSON.stringify(result.ops), undefined, 2);
+  });
+
+  db.close();
 });
